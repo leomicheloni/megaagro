57433de0fe136b02256f6e16e8cb35580be9c4ac
Leonardo Micheloni <leonardo@kinetica-solutions.com>
2014-04-07 20:11:56 -0300

    first draft for factura server first draft for data access interface

---
+++ web/web.services/FacturaService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using web.model;
+using web.data;
+
+namespace web.services
+{
+    public class FacturaService
+    {
+        private IDataAccess dataAccess;
+
+        public FacturaService(IDataAccess dataAccess)
+        {
+            this.dataAccess = dataAccess;
+        }
+
+        public IEnumerable<Factura> GetAll()
+        {
+            return this.dataAccess.Get<Factura>();
+        }
+    }
+}
