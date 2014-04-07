b731a094bbbbcc8a4fcab0a2e318ab915f6ba076
Leonardo Micheloni <leonardo@kinetica-solutions.com>
2014-04-07 20:43:45 -0300

    first draft for login page and usuario controller

---
+++ web/web/Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using web.Models;
+
+namespace web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        //
+        // GET: /Usuario/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// La vista envía los datos para intentar logear al usuario
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login(LoginUserViewModel loginUser)
+        {
+            return new EmptyResult();
+        }
+
+    }
+}
---
+++ web/web/Models/LoginUserViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace web.Models
+{
+    /// <summary>
+    /// Representa un usuario desde el punto de vista de la vista
+    /// en el momento del login
+    /// </summary>
+    public class LoginUserViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
